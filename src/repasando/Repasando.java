9ff36984bb9e7b81f5f5c6bd1256bed438ff84eb
GlenBarbier <glendabarbiero@gmail.com>
2023-10-25 22:05:25 -0300

    Tipos de datos primitivos

--- src/repasando/Repasando.java
+++ src/repasando/Repasando.java
@@ -28,6 +28,29 @@ public class Repasando {
         System.out.println("Me llamo: "+glenNombre);
         System.out.println("Y tengo: "+glenEdad+ " años");
         
+        //TIPOS DE DATOS PRIMITIVOS
+        /*
+        En Java, hay ocho tipos de datos primitivos que se utilizan para representar valores simples, es decir, valores que no son objetos. Estos tipos de datos primitivos son:
+
+1. `byte`: Representa números enteros de 8 bits con signo y tiene un rango de -128 a 127.
+
+2. `short`: Representa números enteros de 16 bits con signo y tiene un rango de -32,768 a 32,767.
+
+3. `int`: Representa números enteros de 32 bits con signo y tiene un rango de -2,147,483,648 a 2,147,483,647. El tipo de dato `int` es uno de los tipos de datos primitivos más comunes en Java.
+
+4. `long`: Representa números enteros de 64 bits con signo y tiene un rango mucho mayor que `int`, que va desde -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807. Para especificar un valor `long`, se debe agregar una "L" al final del número (por ejemplo, `long numeroLargo = 123456789L;`).
+
+5. `float`: Representa números de punto flotante de 32 bits. Se utiliza para representar números decimales y se debe agregar una "f" al final del número (por ejemplo, `float numeroDecimal = 3.14f;`).
+
+6. `double`: Representa números de punto flotante de 64 bits y se utiliza para números decimales con mayor precisión. El tipo de dato `double` es ampliamente utilizado en cálculos científicos y financieros.
+
+7. `char`: Representa un único carácter Unicode de 16 bits. Se utiliza para almacenar caracteres individuales, como letras y símbolos. Los caracteres se deben colocar entre comillas simples (por ejemplo, `char letra = 'A';`).
+
+8. `boolean`: Representa un valor booleano que puede ser `true` o `false`. Se utiliza para realizar evaluaciones lógicas y controlar el flujo de un programa.
+
+Estos tipos de datos primitivos son esenciales en Java y se utilizan para declarar variables y representar diferentes tipos de valores en programas Java.
+        */
+        
         
     }
     
